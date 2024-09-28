29fb2bbf0b8f375cd245aa8d8ad70d37dc51bbb6
pkpw <123902284+pkpw@users.noreply.github.com>
2024-09-27 23:41:23 -0500

    Rename signOut to logout

--- src/routes/(private)/dashboard/+page.svelte
+++ src/routes/(private)/dashboard/+page.svelte
@@ -2,21 +2,20 @@
 	import { goto } from '$app/navigation';
 
 	export let data;
-	$: ({ session, supabase } = data);
+	$: ({ supabase } = data);
 
-	async function signOut() {
+	$: logout = async () => {
 		const { error } = await supabase.auth.signOut();
 		if (error) {
 			console.error(error);
-		} else {
-			goto('/auth');
 		}
-	}
+		goto('/auth');
+	};
 </script>
 
 <svelte:head>
-  <title>Nitin | Dashboard</title>
+	<title>Nitin | Dashboard</title>
 </svelte:head>
 
 <h1>Dashboard</h1>
-<button on:click={signOut}>Sign out</button>
+<button on:click={logout}>Logout</button>
