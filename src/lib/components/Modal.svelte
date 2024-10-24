3651fa80206575849fbbb3e2a95215d436d5df29
pk <123902284+pkpw@users.noreply.github.com>
2024-10-24 16:38:08 -0500

    Prevent modal from hiding by clicking background

--- src/lib/components/Modal.svelte
+++ src/lib/components/Modal.svelte
@@ -16,7 +16,6 @@
 		in:fade={{ delay: 0, duration: 150 }}
 		out:fade={{ delay: 0, duration: 150 }}
 		class="fixed left-0 top-0 z-50 flex h-screen w-screen cursor-default flex-col items-center justify-center overflow-clip bg-stone-950 bg-opacity-80 backdrop-blur-sm"
-		on:click={hide}
 	>
 		<div
 			bind:this={dialog}
