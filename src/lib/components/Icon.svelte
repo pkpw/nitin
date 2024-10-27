322dcba25c4c2c79c0f85a17a22b27449bece017
pk <123902284+pkpw@users.noreply.github.com>
2024-10-26 20:47:12 -0500

    Add hover icons

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -11,6 +11,9 @@
 	let theme = Theme.get();
 	$: dark_mode = Theme.is_dark_mode($theme);
 
+	let hovering = false;
+	$: console.log(hovering ? 'hover enter' : 'hover leave')
+
 	onMount(() => {
 		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
 			dark_mode = Theme.is_dark_mode($theme);
@@ -18,10 +21,12 @@
 	});
 </script>
 
-<picture class="pointer-events-none object-cover">
-	{#if dark_mode}
-		<img src={icon.dark} {alt} {width} {height} />
-	{:else}
-		<img src={icon.light} {alt} {width} {height} />
-	{/if}
-</picture>
+<div role="img" on:mouseenter={() => hovering = true} on:mouseleave={() => hovering = false}>
+	<picture class="pointer-events-none object-cover">
+		{#if dark_mode}
+			<img src={hovering && icon.hover ? icon.hover.dark : icon.dark} {alt} {width} {height} />
+		{:else}
+			<img src={hovering && icon.hover ? icon.hover.light : icon.light} {alt} {width} {height} />
+		{/if}
+	</picture>
+</div>
