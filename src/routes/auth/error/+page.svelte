eedac23eadf2862d9d0b9dcdf0ad598a78db1ce9
pk <123902284+pkpw@users.noreply.github.com>
2024-10-31 21:41:45 -0500

    Add gradient background to auth error page

--- src/routes/auth/error/+page.svelte
+++ src/routes/auth/error/+page.svelte
@@ -1,14 +1,21 @@
+<script>
+	import Background from '$lib/assets/Background.avif';
+</script>
+
 <svelte:head>
 	<title>Nitin | Login Error</title>
 </svelte:head>
 
-<div class="mx-auto flex min-h-screen flex-col items-center justify-center overflow-hidden">
-	<div class="mx-auto max-w-sm p-4">
-		<h1 class="pb-20 text-center text-4xl font-bold">An error occurred.</h1>
-		<div class="mb-24 rounded-md border border-stone-700 p-8 text-center">
-			<h1 class="text-xl font-semibold">Login Failed</h1>
-			<p class="text-lg">We were unable to sign in to your account.</p>
-			<a class="btn-primary mt-4 w-full rounded-full" href="/auth">Try Again</a>
-		</div>
+<div
+	class="mx-auto flex min-h-screen flex-col items-center justify-center overflow-hidden bg-cover bg-center bg-no-repeat px-4"
+	style="background-image: url('{Background}');"
+>
+	<h1 class="pb-16 text-center font-serif text-5xl font-medium">An error occurred.</h1>
+	<div
+		class="mx-auto max-w-80 rounded-xl border border-stone-400 bg-stone-50 bg-opacity-90 p-8 backdrop-blur dark:border-stone-700 dark:bg-stone-950"
+	>
+		<h1 class="text-center text-xl font-semibold">Login Failed</h1>
+		<p class="text-center text-lg">We were unable to sign in to your account.</p>
+		<a class="btn-primary mt-4 w-full rounded-full" href="/auth">Try Again</a>
 	</div>
 </div>
