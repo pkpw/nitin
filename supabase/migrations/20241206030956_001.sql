93faf8136c2ea4525f3f2324236b933b43bc735d
pk <123902284+pkpw@users.noreply.github.com>
2024-12-05 21:22:50 -0600

    Squash migrations

--- supabase/migrations/20241206030956_001.sql
+++ supabase/migrations/20241206030956_001.sql
@@ -101,6 +101,18 @@ SET default_tablespace = '';
 SET default_table_access_method = "heap";
 
 
+CREATE TABLE IF NOT EXISTS "public"."answers" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "question_id" "uuid",
+    "answer" "text",
+    "is_correct" boolean,
+    "order" bigint
+);
+
+
+ALTER TABLE "public"."answers" OWNER TO "postgres";
+
+
 CREATE TABLE IF NOT EXISTS "public"."decks" (
     "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
     "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
@@ -146,6 +158,34 @@ CREATE TABLE IF NOT EXISTS "public"."profiles" (
 ALTER TABLE "public"."profiles" OWNER TO "postgres";
 
 
+CREATE TABLE IF NOT EXISTS "public"."questions" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "quiz_id" "uuid",
+    "question" "text",
+    "order" bigint
+);
+
+
+ALTER TABLE "public"."questions" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."quizzes" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "owner_id" "uuid" DEFAULT "auth"."uid"(),
+    "created_at" timestamp without time zone DEFAULT "now"(),
+    "title" "text",
+    "color" "text"
+);
+
+
+ALTER TABLE "public"."quizzes" OWNER TO "postgres";
+
+
+ALTER TABLE ONLY "public"."answers"
+    ADD CONSTRAINT "answers_pkey" PRIMARY KEY ("id");
+
+
+
 ALTER TABLE ONLY "public"."decks"
     ADD CONSTRAINT "decks_id_key" UNIQUE ("id");
 
@@ -171,6 +211,21 @@ ALTER TABLE ONLY "public"."profiles"
 
 
 
+ALTER TABLE ONLY "public"."questions"
+    ADD CONSTRAINT "questions_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."quizzes"
+    ADD CONSTRAINT "quizzes_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."quizzes"
+    ADD CONSTRAINT "unique_quiz_title_per_owner" UNIQUE ("owner_id", "title");
+
+
+
 CREATE INDEX "decks_owner_id_idx" ON "public"."decks" USING "hash" ("owner_id");
 
 
@@ -184,6 +239,11 @@ CREATE UNIQUE INDEX "unique_deck_title" ON "public"."decks" USING "btree" ("owne
 
 
 CREATE UNIQUE INDEX "unique_flashcard_title" ON "public"."flashcards" USING "btree" ("deck_id", "title");
+
+
+
+ALTER TABLE ONLY "public"."answers"
+    ADD CONSTRAINT "answers_question_id_fkey" FOREIGN KEY ("question_id") REFERENCES "public"."questions"("id") ON DELETE CASCADE;
 
 
 
@@ -202,6 +262,83 @@ ALTER TABLE ONLY "public"."profiles"
 
 
 
+ALTER TABLE ONLY "public"."questions"
+    ADD CONSTRAINT "questions_quiz_id_fkey" FOREIGN KEY ("quiz_id") REFERENCES "public"."quizzes"("id") ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."quizzes"
+    ADD CONSTRAINT "quizzes_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "public"."profiles"("id");
+
+
+
+CREATE POLICY "Answers policy" ON "public"."answers" USING (true);
+
+
+
+CREATE POLICY "Enable delete for owners" ON "public"."quizzes" FOR DELETE USING (("auth"."uid"() = "owner_id"));
+
+
+
+CREATE POLICY "Enable delete for quiz owners" ON "public"."answers" FOR DELETE USING ((EXISTS ( SELECT 1
+   FROM ("public"."questions" "q"
+     JOIN "public"."quizzes" "qz" ON (("q"."quiz_id" = "qz"."id")))
+  WHERE (("q"."id" = "answers"."question_id") AND ("qz"."owner_id" = "auth"."uid"())))));
+
+
+
+CREATE POLICY "Enable delete for quiz owners" ON "public"."questions" FOR DELETE USING ((EXISTS ( SELECT 1
+   FROM "public"."quizzes"
+  WHERE (("quizzes"."id" = "questions"."quiz_id") AND ("quizzes"."owner_id" = "auth"."uid"())))));
+
+
+
+CREATE POLICY "Enable insert for authenticated users only" ON "public"."quizzes" FOR INSERT WITH CHECK (("auth"."uid"() = "owner_id"));
+
+
+
+CREATE POLICY "Enable insert for quiz owners" ON "public"."answers" FOR INSERT WITH CHECK ((EXISTS ( SELECT 1
+   FROM ("public"."questions" "q"
+     JOIN "public"."quizzes" "qz" ON (("q"."quiz_id" = "qz"."id")))
+  WHERE (("q"."id" = "answers"."question_id") AND ("qz"."owner_id" = "auth"."uid"())))));
+
+
+
+CREATE POLICY "Enable insert for quiz owners" ON "public"."questions" FOR INSERT WITH CHECK ((EXISTS ( SELECT 1
+   FROM "public"."quizzes"
+  WHERE (("quizzes"."id" = "questions"."quiz_id") AND ("quizzes"."owner_id" = "auth"."uid"())))));
+
+
+
+CREATE POLICY "Enable read access for all users" ON "public"."answers" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Enable read access for all users" ON "public"."questions" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Enable read access for all users" ON "public"."quizzes" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Enable update for owners" ON "public"."quizzes" FOR UPDATE USING (("auth"."uid"() = "owner_id"));
+
+
+
+CREATE POLICY "Enable update for quiz owners" ON "public"."answers" FOR UPDATE USING ((EXISTS ( SELECT 1
+   FROM ("public"."questions" "q"
+     JOIN "public"."quizzes" "qz" ON (("q"."quiz_id" = "qz"."id")))
+  WHERE (("q"."id" = "answers"."question_id") AND ("qz"."owner_id" = "auth"."uid"())))));
+
+
+
+CREATE POLICY "Enable update for quiz owners" ON "public"."questions" FOR UPDATE USING ((EXISTS ( SELECT 1
+   FROM "public"."quizzes"
+  WHERE (("quizzes"."id" = "questions"."quiz_id") AND ("quizzes"."owner_id" = "auth"."uid"())))));
+
+
+
 CREATE POLICY "Owners can modify their flashcard decks." ON "public"."decks" USING ((( SELECT "auth"."uid"() AS "uid") = "owner_id"));
 
 
@@ -209,6 +346,14 @@ CREATE POLICY "Owners can modify their flashcard decks." ON "public"."decks" USI
 CREATE POLICY "Owners of a flashcard deck can modify the flashcards in it." ON "public"."flashcards" USING (("deck_id" IN ( SELECT "decks"."id"
    FROM "public"."decks"
   WHERE ("decks"."owner_id" = ( SELECT "auth"."uid"() AS "uid")))));
+
+
+
+CREATE POLICY "Questions policy" ON "public"."questions" USING (true);
+
+
+
+CREATE POLICY "Quizzes policy" ON "public"."quizzes" USING (("auth"."uid"() = "owner_id"));
 
 
 
@@ -224,8 +369,41 @@ CREATE POLICY "Users can delete a profile." ON "public"."profiles" FOR DELETE TO
 
 
 
+CREATE POLICY "Users can manage answers" ON "public"."answers" USING (("auth"."uid"() IN ( SELECT "quizzes"."owner_id"
+   FROM "public"."quizzes"
+  WHERE ("quizzes"."id" = ( SELECT "questions"."quiz_id"
+           FROM "public"."questions"
+          WHERE ("questions"."id" = "answers"."question_id"))))));
+
+
+
+CREATE POLICY "Users can manage questions" ON "public"."questions" USING (("auth"."uid"() IN ( SELECT "quizzes"."owner_id"
+   FROM "public"."quizzes"
+  WHERE ("quizzes"."id" = "questions"."quiz_id"))));
+
+
+
+CREATE POLICY "Users can manage their own quizzes" ON "public"."quizzes" USING (("auth"."uid"() = "owner_id"));
+
+
+
 CREATE POLICY "Users can update their own profile." ON "public"."profiles" FOR UPDATE TO "authenticated" USING ((( SELECT "auth"."uid"() AS "uid") = "id")) WITH CHECK ((( SELECT "auth"."uid"() AS "uid") = "id"));
 
+
+
+CREATE POLICY "Users can view answers" ON "public"."answers" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Users can view questions" ON "public"."questions" FOR SELECT USING (true);
+
+
+
+CREATE POLICY "Users can view quizzes" ON "public"."quizzes" FOR SELECT USING (true);
+
+
+
+ALTER TABLE "public"."answers" ENABLE ROW LEVEL SECURITY;
 
 
 ALTER TABLE "public"."decks" ENABLE ROW LEVEL SECURITY;
@@ -235,6 +413,12 @@ ALTER TABLE "public"."flashcards" ENABLE ROW LEVEL SECURITY;
 
 
 ALTER TABLE "public"."profiles" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."questions" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."quizzes" ENABLE ROW LEVEL SECURITY;
 
 
 
@@ -456,6 +640,12 @@ GRANT ALL ON FUNCTION "public"."handle_new_user"() TO "service_role";
 
 
 
+GRANT ALL ON TABLE "public"."answers" TO "anon";
+GRANT ALL ON TABLE "public"."answers" TO "authenticated";
+GRANT ALL ON TABLE "public"."answers" TO "service_role";
+
+
+
 GRANT ALL ON TABLE "public"."decks" TO "anon";
 GRANT ALL ON TABLE "public"."decks" TO "authenticated";
 GRANT ALL ON TABLE "public"."decks" TO "service_role";
@@ -471,6 +661,18 @@ GRANT ALL ON TABLE "public"."flashcards" TO "service_role";
 GRANT ALL ON TABLE "public"."profiles" TO "anon";
 GRANT ALL ON TABLE "public"."profiles" TO "authenticated";
 GRANT ALL ON TABLE "public"."profiles" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."questions" TO "anon";
+GRANT ALL ON TABLE "public"."questions" TO "authenticated";
+GRANT ALL ON TABLE "public"."questions" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."quizzes" TO "anon";
+GRANT ALL ON TABLE "public"."quizzes" TO "authenticated";
+GRANT ALL ON TABLE "public"."quizzes" TO "service_role";
 
 
 
